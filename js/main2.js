a1cfdd5f8047eaf78a0550fbf3f26a6b3035036a
nithinsha99 <nithinshacoder99@gmail.com>
2024-01-26 23:09:46 +0530

    first commit

---
+++ js/main2.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+   
+
+
+    // Related carousel
+    $(".related-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
